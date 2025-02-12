069f3485d0ef5c4050a380faa085bc894633e89a
sean dougherty <sd@kexp.org>
2025-02-11 18:01:31 -1000

    archiving works. save data, click done. moves to archive. button to see archive. restore from there moves it back to index.

--- preload.js
+++ preload.js
@@ -2,5 +2,9 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
     saveRow: (data) => ipcRenderer.invoke("saveRow", data),
-    deleteRow: (id) => ipcRenderer.invoke("deleteRow", id), // Add this
+    deleteRow: (id) => ipcRenderer.invoke("deleteRow", id),
+    archiveRow: (id, isArchived) => ipcRenderer.invoke("archiveRow", id, isArchived),
+    getArchivedRows: () => ipcRenderer.invoke("getArchivedRows"),
+    getActiveRows: () => ipcRenderer.invoke("getActiveRows"), // NEW FUNCTION
 });
+
