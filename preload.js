7a080cd41598ed9a02cf187ea5680ba0562db5c8
sean dougherty <sd@kexp.org>
2025-02-12 11:43:10 -1000

    updated db to add a deleted flag/field.
    delete an item from index or archive and it goes to deleted.html. depending on if the thing was deleted from index or html is where it will restore to.

--- preload.js
+++ preload.js
@@ -5,5 +5,7 @@ contextBridge.exposeInMainWorld("electronAPI", {
     deleteRow: (id) => ipcRenderer.invoke("deleteRow", id),
     archiveRow: (id, isArchived) => ipcRenderer.invoke("archiveRow", id, isArchived),
     getArchivedRows: (sortBy, sortOrder) => ipcRenderer.invoke("getArchivedRows", sortBy, sortOrder),
-    getActiveRows: () => ipcRenderer.invoke("getActiveRows"), // ✅ This is correct
+    getActiveRows: () => ipcRenderer.invoke("getActiveRows"),
+    getDeletedRows: (sortBy, sortOrder) => ipcRenderer.invoke("getDeletedRows", sortBy, sortOrder), // ✅ Ensure this is here
+    restoreDeletedRow: (id) => ipcRenderer.invoke("restoreDeletedRow", id),
 });
